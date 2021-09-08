d9c5fc1cd85492cc164bf578b141212b505e64d6
Caio Cordeiro <ccmr@cesar.org.br>
2021-09-08 16:11:06 -0300

    feat: add healthcare dashboard v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Card from "./components/Card";
+import StatusBall from "./components/StatusBall";
+import elderly_f from "./assets/icons/elderly_f.png";
+import elderly_help from "./assets/icons/elderly_help.png";
+import heartBeat from "./assets/icons/heart-beat.png";
+import thermometer from "./assets/icons/thermometer.png";
+import gyroscope from "./assets/icons/gyroscope.png";
+import blood_pressure from "./assets/icons/blood-pressure.png";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <div className="App">
+        <div className="header">
+          <Card bgColor="#7F67FB">
+            <div className="card-content">
+              <img className="profile-img" src={elderly_f} />
+              <p>Lurdes Da Silva</p>
+              <p>95</p>
+            </div>
+          </Card>
+          <div className="help-status">
+            <img className="help-img" src={elderly_help} />
+            <StatusBall status="critical" />
+          </div>
+        </div>
+        <div className="cards">
+          <div className="card-container">
+            <Card bgColor="white">
+              <div className="card-content">
+                <img className="card-img" src={heartBeat} />
+                <p>95</p>
+                <p>bpm</p>
+                <div className="status">
+                  <StatusBall variant="small" status="normal" />
+                </div>
+              </div>
+            </Card>
+          </div>
+          <div className="card-container">
+            <Card bgColor="white">
+              <div className="card-content">
+                <img className="card-img" src={blood_pressure} />
+                <p>95</p>
+                <p>bpm</p>
+                <div className="status">
+                  <StatusBall variant="small" status="warning" />
+                </div>
+              </div>
+            </Card>
+          </div>
+          <div className="card-container">
+            <Card bgColor="white">
+              <div className="card-content">
+                <img className="card-img" src={gyroscope} />
+                <p>95</p>
+                <p>bpm</p>
+                <div className="status">
+                  <StatusBall variant="small" status="worrying" />
+                </div>
+              </div>
+            </Card>
+          </div>
+          <div className="card-container">
+            <Card bgColor="white">
+              <div className="card-content">
+                <img className="card-img" src={thermometer} />
+                <p>95</p>
+                <p>bpm</p>
+                <div className="status">
+                  <StatusBall variant="small" status="critical" />
+                </div>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </React.StrictMode>
   );
 }
 
